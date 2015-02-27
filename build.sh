3a4d221701c48489e1f5d9e70bc382443739e716
b-sh <b-sh@gmx.net>
2015-02-28 00:02:18 +0100

    Add keep alive during build if something not delivering output for more than 10 minutes

---
+++ build.sh
@@ -0,0 +1,23 @@
+keep_alive() {
+	while true
+	do
+		# this is for telling travis not to kill the job when no output happens for long time
+		# which can happen during bitbake build
+		echo "Running please not kill!"
+		sleep 60
+	done
+}
+
+run_build() {
+	repo init -b yocto/daisy-freescale -u https://github.com/bor-sh/ros-embedded-manifest.git
+	repo sync
+	TEMPLATECONF=meta-templates/conf/freescale source poky/oe-init-build-env yocto-fslc
+	bitbake core-image-base	
+	ret=$?
+	kill $keep_alive_pid >/dev/null 2>&1
+	return $ret
+}
+
+keep_alive &
+keep_alive_pid=$!
+run_build
